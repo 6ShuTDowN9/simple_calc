d1d6b489b9fe3bc8f26b00b04ae9c82f83f70ab2
6ShuTDowN9 <bekaistileu@gmail.com>
2024-06-07 00:04:27 +0600

    added main file

---
+++ main.py
@@ -0,0 +1,6 @@
+import customtkinter
+calc_window = customtkinter.CTk()
+calc_window.geometry("350x400")
+button = customtkinter.CTkButton(calc_window, text = "hello kitty", width=100, height=80)
+button.pack()
+calc_window.mainloop()
